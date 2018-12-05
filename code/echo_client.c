2839e0effc8f824c0c36cc4846893ce64ac12f7f
Kilian Pfeiffer <k.pfeiffer@tum.de>
2018-12-05 13:03:42 -0800

    client added

---
+++ code/echo_client.c
@@ -0,0 +1,22 @@
+#include <u.h>
+#include <libc.h>
+
+void main(int argc, char *argv[])
+{
+    char buf[256] = {"a"};
+    int n;
+
+    int fd = dial("tcp!127.0.0.1!7",0,0,0);
+
+    if(fd == -1)
+    {
+        print("failed\n");
+        exits(nil);
+    }
+
+    write(fd,buf,sizeof(buf));
+    while((n = read(fd,buf,sizeof(buf)))> 0);
+
+    print("reading from buffer\n");
+    exits(nil);
+}
